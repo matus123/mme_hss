9f5504420c427f0cd5a5520a28dfeb37b740775c
Matus Kislan <matuskislan@gmail.com>
2016-04-20 18:16:48 +0200

    added logger dependency

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 'use strict';
 
 const server = require('./server/server');
+const logger = require('./logger');
 
 const HOST = process.env.HOST || '127.0.0.1';
 const PORT = process.env.PORT || 3868;
